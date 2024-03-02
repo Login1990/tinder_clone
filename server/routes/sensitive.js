03ed74054b744118e2c2b032ce8a8387ada022bc
Login1990 <79404334+Login1990@users.noreply.github.com>
2024-03-02 12:51:46 +0200

    Updated middleware

--- server/routes/sensitive.js
+++ server/routes/sensitive.js
@@ -1,16 +1,19 @@
 // Import necessary modules
 const express = require('express');
 const router = express.Router();
+const jwt = require("jsonwebtoken")
 
 // Middleware function for authorization
 const authorize = (req, res, next) => {
-    // Check if user is authorized
-    if (req.headers.authorization === 'mySecretToken') {
-        // If authorized, proceed to the next middleware
-        next();
-    } else {
-        // If not authorized, send 403 Forbidden status
-        res.sendStatus(403);
+    const token = req.headers.authorization
+    if (token && process.env.SECRET){
+      jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if(err){
+          res.status(403).json({error: "Token verification failed. :("})
+        } else {
+          next()
+        }
+      })
     }
 };
 
